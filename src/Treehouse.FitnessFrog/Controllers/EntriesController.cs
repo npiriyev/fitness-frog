74ccfe5aa8b5cbcd7aa283b24746286792b19400
lighto <npiriyev@live.com>
2018-02-25 21:32:28 +0200

    2nd

--- src/Treehouse.FitnessFrog/Controllers/EntriesController.cs
+++ src/Treehouse.FitnessFrog/Controllers/EntriesController.cs
@@ -6,7 +6,7 @@ using System.Web;
 using System.Web.Mvc;
 using Treehouse.FitnessFrog.Data;
 using Treehouse.FitnessFrog.Models;
-using System.Dynamic;
+
 
 namespace Treehouse.FitnessFrog.Controllers
 {
